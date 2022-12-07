05f62065cf3205c2768f837ef4e67eb87e5792d8
Gaetan Poggi <gaetan.poggi.gp@gmail.com>
2022-12-07 18:04:07 +0100

    2nd commit with variables

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // This file contains the logic for a randomized message generator
+const adjectivesCata = ['sunshine', 'angel', 'sweetheart', 'darling', 'memecita', 'beautiful']
+const greetings = ['Goog morning', 'Hello', 'Hola', 'Hey']
+const adjectivesDay = ['an amazing', 'a great', 'a']
